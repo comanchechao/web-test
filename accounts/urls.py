c98a1da724988ec2475eae0b8bb39d3a85a81b3f
comanchechao <chao.comanche@gmail.com>
2020-04-05 05:00:52 +0430

    First commit

---
+++ accounts/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+
+    url(r'^signup/$', views.signup_view, name="signup"),
+    url(r'^login/$', views.login_view, name="login"),
+    url(r'^logout/$', views.logout_view, name="logout"),
+    url(r'^passwordchange/$', views.passwordchange_view, name="passwordchange"),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
+]
---
+++ accounts/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import login, logout, update_session_auth_hash
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .forms import UserSignUpForm
+from .token import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+
+
+def signup_view(request):
+    if request.method == 'POST':
+         form = UserCreationForm(request.POST)
+         if form.is_valid():
+             user = form.save(commit=False)
+             user.is_active = False
+             user.save()
+             current_site = get_current_site(request)
+             email_subject = 'Activate Your Account'
+             message = render_to_string('activate_account.html',
+             {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
+                    'token': account_activation_token.make_token(user),
+             })
+
+             to_email = form.cleaned_data.get('email')
+             email = EmailMessage(email_subject, message, to=[to_email])
+             email.send()
+             return HttpResponse('We have sent you an email, please confirm your email address to complete registration')
+             login(request, user)
+             if 'next' in request.POST:
+                 return redirect(request.POST.get('next'))
+             else:
+                 return redirect('articles:list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', { 'form': form })
+
+def activate(request, uidb64, token):
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(id=uid)
+        except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+        if user is not None and account_activation_token.check_token(user, token):
+            user.is_active = True
+            user.save()
+            return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+        else:
+            return HttpResponse('Activation link is invalid!')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('articles:list')
+
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', { 'form': form })
+
+def logout_view(request):
+    if request.method == 'POST':
+            logout(request)
+            return redirect('articles:list')
+
+def passwordchange_view(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('passwordchange')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'accounts/passwordchange.html', {'form': form })
