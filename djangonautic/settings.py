90e6652faa1e71bd3f36ff44cca01dbe2c33d6c5
comanchechao <61477751+comanchechao@users.noreply.github.com>
2020-04-07 06:07:02 +0430

    Update settings.py
    Added a couple of comments

--- djangonautic/settings.py
+++ djangonautic/settings.py
@@ -129,7 +129,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-
+# Might be the problem
+# Check if it successfuly sends an email
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
