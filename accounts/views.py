38feaa18e0b968593ddc98a21834d49579532c85
comanchechao <61477751+comanchechao@users.noreply.github.com>
2020-04-20 16:35:37 +0430

    couple of changes

--- accounts/views.py
+++ accounts/views.py
@@ -72,7 +72,7 @@ def login_view(request):
 def logout_view(request):
     if request.method == 'POST':
             logout(request)
-            return redirect('articles:list')
+            return redirect('templates/homepage.html')
 
 def passwordchange_view(request):
     if request.method == 'POST':
